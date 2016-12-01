1d80f8dac2c94fd334daef46161b5509d9d8a58f
Paul Foster <paulfo@microsoft.com>
2016-12-01 09:45:52 +0000

    Inital Stub Web API facade for services used by LUIS Bot

---
+++ LUISHack2016/LUISHack2016/Controllers/ValuesController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace LUISHack2016.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+        public string Get(string location)
+        {
+            // Looking up the NR Darwin information for location value.
+
+            return "Cambridge";
+        }
+
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ LUISHack2016/LUISHack2016/obj/Release/Package/PackageTmp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LUISHack2016.WebApiApplication" Language="C#" %>
